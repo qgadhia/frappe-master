17c5d64b91d159c050f307e42deb9321ed77eca6
Administrator <gitlab_admin_26f219@example.com>
2025-08-01 09:59:35 +0000

    Edit deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -8,17 +8,13 @@ echo "[DEBUG] CI_COMMIT_SHA: ${CI_COMMIT_SHA:-missing}"
 echo "[DEBUG] DOCKER_REGISTRY_URL: ${DOCKER_REGISTRY_URL:-missing}"
 
 # Path setup
-APP_DIR="/home/indosoltm/cicd"
+APP_DIR="/home/$VM_USER/cicd"
 COMPOSE_FILE="$APP_DIR/docker-compose.yml"
 
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin $DOCKER_REGISTRY_URL
 
 echo "🔄 Changing to app directory: $APP_DIR"
 cd "$APP_DIR"
-
-# Stop existing stack
-echo "🛑 Bringing down existing containers..."
-docker compose -f docker-compose.yml down --volumes || true
 
 # Replace placeholder in compose file
 echo "✏️  Replacing CI_COMMIT_SHA in compose file..."
@@ -28,6 +24,10 @@ sed -i -e "s/\\\$CI_COMMIT_SHA/${CI_COMMIT_SHA}/g" "$COMPOSE_FILE"
 echo "⬇️  Pulling latest images..."
 docker compose -f "$COMPOSE_FILE" pull --quiet
 
+# Stop existing stack
+echo "🛑 Bringing down existing containers..."
+docker compose -f docker-compose.yml down || true
+	
 # Start the stack
 echo "🚀 Starting containers..."
 docker compose -f "$COMPOSE_FILE" up -d --quiet-pull --no-build
